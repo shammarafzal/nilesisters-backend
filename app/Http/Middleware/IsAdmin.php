1df92f0ca4dd69a8a2610e4fe656bc065966f6c5
Muhammad Rana <muhammadrana523@gmail.com>
2021-07-20 13:18:50 +0500

    Changes

--- app/Http/Middleware/IsAdmin.php
+++ app/Http/Middleware/IsAdmin.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class IsAdmin
 {
@@ -19,7 +20,9 @@ class IsAdmin
         if(auth()->check() && auth()->user()->is_admin == 1){
             return $next($request);
         }
-
+        Auth::guard('web')->logout();
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
         return redirect('/')->with('error',"You don't have admin access.");
     }
 }
