b132d52ea64980460dd207659114f8f0bd7da72b
Muhammad Rana <muhammadrana523@gmail.com>
2021-07-20 12:31:07 +0500

    Is admin check

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -18,6 +18,7 @@ class UsersTableSeeder extends Seeder
         User::create([
             'name' => 'Admin',
             'email' => 'admin@admin.com',
+            'is_admin' => 1,
             'password' => Hash::make('password'),
         ]);
     }
