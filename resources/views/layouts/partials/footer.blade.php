265ae6c35aefe1ad3157d88406df28d8284eb503
Ammar Afzal <ammarafzal075@gmail.com>
2022-03-30 18:19:08 +0500

    Fix footer

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -1,3 +1,3 @@
 <footer class="footer text-center text-sm-left">
-    &copy; 2021 Nilesisters <span class="d-none d-sm-inline-block float-right">All Rights Reserved</span>
+    &copy; 2022 Nilesisters <span class="d-none d-sm-inline-block float-right">All Rights Reserved</span>
 </footer>
